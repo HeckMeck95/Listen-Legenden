781e4a22532de99871efed5b9b1939bcd5e4fdef
HeckMeck <76648652+HeckMeck95@users.noreply.github.com>
2026-05-31 02:59:36 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,15 @@
 @echo off
 cd /d "%~dp0"
+
+echo.
+echo Installiere Abhaengigkeiten fuer Listen-Legenden...
+echo.
+
 npm install
+
+echo.
+echo Installation abgeschlossen.
+echo Du kannst die App jetzt mit start.bat starten.
+echo.
+
 pause
